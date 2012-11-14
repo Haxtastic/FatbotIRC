21ab951c9ada1a98c3da5ea9498479c569892b90
Koerveter <magz1337+github@gmail.com>
2012-11-14 10:55:30 +0100

    Added Guessnumber module.

--- moduleloader.py
+++ moduleloader.py
@@ -4,4 +4,5 @@ def load_modules(evManager):
 	list = []
 	#  Load your modules here
 	list.append(protocolirc.protocolIRC(evManager))
+	list.append(guessnumber.GuessnumberManager(evManager))
 	return list
