871ae0437d6c17b80af9b9ff37aa15b06a48dfc0
Koerveter <magz1337+github@gmail.com>
2012-11-16 17:24:24 +0100

    Added youtubehelper module.

--- moduleloader.py
+++ moduleloader.py
@@ -5,4 +5,5 @@ def load_modules(evManager):
 	#  Load your modules here
 	list.append(protocolirc.protocolIRC(evManager))
 	list.append(gamemanager.GameManager(evManager))
+	list.append(youtubehelper.youtubeHelper(evManager))
 	return list
