a10f13a2f5b953552383e9d992e5cba27fd471c7
Koerveter <magz1337+github@gmail.com>
2012-11-18 13:12:37 +0100

    Added error handling and moved output string to config.

--- modules/youtubehelper.py
+++ modules/youtubehelper.py
@@ -6,26 +6,41 @@ import gdata.youtube
 import gdata.youtube.service
 import urlparse
 import re
+import ConfigParser
 
 class youtubeHelper():
 	def __init__(self, evManager):
 		self.evManager = evManager
-		self.evManager.register_listener(self)
+		self.read_config()
 		self.client = gdata.youtube.service.YouTubeService()
 		self.client.ssl = True
+		self.evManager.register_listener(self)
 		
 	def parse_privmsg(self, event):
 		message = event.message
-		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+		urls = re.findall('(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
 		for url in urls:
 			if url.find("youtube.com/watch") != -1:
 				url_data = urlparse.urlparse(url)
 				query = urlparse.parse_qs(url_data.query)
 				videoid = query["v"][0]
-				video = self.client.GetYouTubeVideoEntry(video_id=videoid)
-				title = "YouTube Title: %s | Viewed: %s times | Rating: %s" % (video.media.title.text, video.statistics.view_count, video.rating.average)
+				try:
+					video = self.client.GetYouTubeVideoEntry(video_id=videoid)
+					title =  self.title % (video.media.title.text, video.statistics.view_count, video.rating.average)
+				except gdata.service.RequestError:
+					title = "YouTube Error: Invalid video_id"
 				self.evManager.post(SendPrivmsgEvent(event.channel, title))
 				
 	def notify(self, event):
 		if isinstance(event, PrivmsgEvent):
 			self.parse_privmsg(event)
+		elif isinstance(event, ReloadconfigEvent):
+			if event.module == "youtubehelper" or event.module == "all":
+				self.read_config()
+			
+	def read_config(self):
+		self.config = ConfigParser.RawConfigParser()
+		self.config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'modules.cfg'))
+		self.title = self.config.get("youtubehelper", "titlemessage")
+		self.title = self.title.replace("\\x02", "\x02").replace("\\x03", "\x03").replace("\\x17", "\x17")
+			
