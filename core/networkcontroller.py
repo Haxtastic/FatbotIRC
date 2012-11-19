82d9d388eb25995ba8867057cd977a18eab52614
Koerveter <magz1337+github@gmail.com>
2012-11-19 15:19:36 +0100

    Added break on newline so bot processes everything.

--- core/networkcontroller.py
+++ core/networkcontroller.py
@@ -9,14 +9,15 @@ class NetworkController():
 		
 	def on_recv_message(self, msg):
 		msg.strip_message()
-		self.parse_packet(msg)
+		messages = msg.buffer.split("\n")
+		for text in messages:
+			self.parse_packet(text)
 		
-	def parse_packet(self, recv):
+	def parse_packet(self, buffer):
 		#source, command, body
 		y, m, d, h, m, s, wd, yd, isdst = time.localtime()
-		self.evManager.post(ConsoleEvent("%02d:%02d:%02d %s" % (h, m, s, recv.buffer)))
-		#print "recv.buffer"
-		parameters = recv.buffer.split(" ", 3)
+		self.evManager.post(ConsoleEvent("%02d:%02d:%02d %s" % (h, m, s, buffer)))
+		parameters = buffer.split(" ", 3)
 		if parameters[0].find(":") != -1:
 			parameters[0] = parameters[0].split(":")[1]  # Get rid of the : at the start of the message
 		if parameters[0] == "PING":
@@ -30,6 +31,9 @@ class NetworkController():
 			except TypeError:
 				error = "Error: [NetworkController::parse_packet] TypeError when formating parameters string"
 			self.evManager.post(ConsoleEvent(error))
+		#Welcome message
+		elif parameters[1] == "001":  # message
+			self.evManager.post(WelcomeEvent(parameters[3]))
 		elif parameters[1] == "PRIVMSG":  # Source, channel, message
 			self.evManager.post(PrivmsgEvent(parameters[0], parameters[2], parameters[3]))
 
