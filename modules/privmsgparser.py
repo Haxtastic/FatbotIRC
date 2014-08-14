f0b955fe88b1754054137d795234c9d6e6731f31
Koerveter <magz1337@gmail.com>
2014-08-14 06:51:55 +0200

    NEW FILEgit add modules/privmsgparser.py! This module handles the message a little bit before modules who want it get it.

---
+++ modules/privmsgparser.py
@@ -0,0 +1,23 @@
+import os, sys
+lib_path = os.path.abspath(os.path.join("..", "core"))
+sys.path.append(lib_path)
+from events import *
+import ConfigParser
+from weakboundmethod import WeakBoundMethod as Wbm
+
+class privmsgparser():
+	def __init__(self, ed):
+		self.ed = ed
+		self._connections = [
+			self.ed.add(PrivmsgEvent, Wbm(self.parse_privmsg)),
+		]
+		
+	def parse_privmsg(self, event):
+		if event.message.find(" ") == -1:
+			return
+		nick, source = event.source.split("!")
+		command = event.message.split(" ")
+		parameters = command[1:]
+		command = command[0].split(":")[1].lower()  # Get rid of the : at start and no caps
+		message = event.message.split(":")[1]
+		self.ed.post(ParsedPrivmsgEvent(nick, source, event.channel, message, command, parameters))
